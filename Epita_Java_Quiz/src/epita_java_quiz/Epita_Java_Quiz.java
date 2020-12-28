b28012bdbd57e1e90bc316c28e73dfb63efd82c4
Vadson <vadrama.ndisang@urpharm.com>
2020-12-28 13:47:40 +0100

    Created java project

---
+++ Epita_Java_Quiz/src/epita_java_quiz/Epita_Java_Quiz.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package epita_java_quiz;
+
+/**
+ *
+ * @author Alcatras
+ */
+public class Epita_Java_Quiz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
