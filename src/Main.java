0ec953a9dd9067bb980fc6611bcdb896f12e7c99
Vadson <vadrama.ndisang@urpharm.com>
2020-12-28 17:05:13 +0100

    Added connection class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import epita.fr.java.quiz.connection.DatabaseConnection;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("EPITA JAVA QUIZ By VADRAMA NDISANG NGYIBI!");
+        DatabaseConnection.getConnection();
     }
 }
---
+++ src/epita/fr/java/quiz/connection/DatabaseConnection.java
@@ -0,0 +1,31 @@
+package epita.fr.java.quiz.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    public static String connectionURL = "jdbc:postgresql://localhost:5432/Epita_Java_Quiz";
+    public static String databaseUser = "postgres";
+    public static String databasePassword = "Vadson";
+    public static Connection connection;
+    public static String connectionDriver = "org.postgresql.Driver";
+
+    public static Connection getConnection() {
+        Connection  con = null;
+        try {
+            Class.forName(connectionDriver);
+            try {
+                 con = DriverManager.getConnection(connectionURL, databaseUser, databasePassword);
+                 if(con != null) System.out.println("Connected!");
+            } catch (SQLException ex) {
+                System.out.println("Failed to create the database connection.");
+            }
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Driver not found.");
+        }
+        return con;
+    }
+
+}
